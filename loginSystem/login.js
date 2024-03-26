17e4a66c2921cf2e7c1d94cf2da1669dcd07a898
johnde626 <159606100+johnde626@users.noreply.github.com>
2024-03-25 21:29:01 -0400

    Update login.js

--- loginSystem/login.js
+++ loginSystem/login.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Initialize Firebase Admin SDK with appropriate credentials
+const serviceAccount = require('./fireBasekey.json'); //link your own FireBase private key
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://pharmpath-6af52-default-rtdb.firebaseio.com"
+});
+const db = admin.firestore();
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
